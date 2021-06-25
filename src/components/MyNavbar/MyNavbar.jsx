26d7a4d17426c5d31e023bac11720496879b707b
John-Mancini <mancinijohn5988@gmail.com>
2021-06-25 17:46:28 -0400

    added resume button

--- src/components/MyNavbar/MyNavbar.jsx
+++ src/components/MyNavbar/MyNavbar.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
 
 function MyNavbar() {
   return (
     <>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
+        <Navbar.Brand href="#home">John Mancini</Navbar.Brand>
+        <Nav className="ml-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#aboutMe">About Me</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href="#contactMe">Contact Me</Nav.Link>
+          <Button variant="outline-info" target="_blank" href="">
+            Resume
+          </Button>
         </Nav>
       </Navbar>
     </>
