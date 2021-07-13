2d75127baf75d0da6316198de56dad9992055fdf
John-Mancini <mancinijohn5988@gmail.com>
2021-07-13 18:36:59 -0400

    added all projects

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -1,44 +1,51 @@
 import React from "react";
+import "./MyProjects.css";
+import Bet from "./assets/bet-fullstack.png";
+import Employee from "./assets/employee-directory.png";
+import Note from "./assets/note-taker.png";
+import Weather from "./assets/weather-api.png";
+import Password from "./assets/password-generator.png";
+import Workout from "./assets/workout-tracker.png";
 let URLS = [
   "https://workout-tracker-john-mancini.herokuapp.com/",
   "https://john-mancini.github.io/weather-api/",
 ];
 let projectData = [
   {
-    title: "Workout-Tracker",
-    link: "https://workout-tracker-john-mancini.herokuapp.com/",
-    repo: "https://github.com/John-Mancini/workout-tracker",
-    image: "workout-tracker.png",
+    title: "Employee Directory",
+    link: "https://employee-directory-jm.herokuapp.com/",
+    repo: "https://github.com/John-Mancini/employee-directory",
+    image: Employee,
   },
   {
-    title: "Workout-Tracker",
-    link: "https://workout-tracker-john-mancini.herokuapp.com/",
-    repo: "https://github.com/John-Mancini/workout-tracker",
-    image: "workout-tracker.png",
+    title: "Password-Generator",
+    link: "https://john-mancini.github.io/Password-Generator/",
+    repo: "https://github.com/John-Mancini/Password-Generator",
+    image: Password,
   },
   {
-    title: "Workout-Tracker",
-    link: "https://workout-tracker-john-mancini.herokuapp.com/",
-    repo: "https://github.com/John-Mancini/workout-tracker",
-    image: "workout-tracker.png",
-  },
-  {
-    title: "Workout-Tracker",
-    link: "https://workout-tracker-john-mancini.herokuapp.com/",
-    repo: "https://github.com/John-Mancini/workout-tracker",
-    image: "workout-tracker.png",
+    title: "Bet Fullstack",
+    link: "https://thereal-baitjet.github.io/bet-fullstack/",
+    repo: "https://github.com/thereal-baitjet/bet-fullstack",
+    image: Bet,
   },
   {
     title: "Weather-Api",
     link: "https://john-mancini.github.io/weather-api/",
     repo: "https://github.com/John-Mancini/weather-api",
-    image: "workout-tracker.png",
+    image: Weather,
   },
   {
     title: "Workout-Tracker",
     link: "https://workout-tracker-john-mancini.herokuapp.com/",
     repo: "https://github.com/John-Mancini/workout-tracker",
-    image: "workout-tracker.png",
+    image: Workout,
+  },
+  {
+    title: "Notes-App",
+    link: "https://note-taker-j.herokuapp.com/",
+    repo: "https://github.com/John-Mancini/Notes-Appr",
+    image: Note,
   },
 ];
 function MyProjects() {
@@ -55,7 +62,12 @@ function Project(props) {
   return (
     <div className="col-6">
       <div className="card">
-        <img src={props.data.image} className="card-img-top" alt="..." />
+        <img
+          id="images"
+          src={props.data.image}
+          className="card-img-top"
+          alt="..."
+        />
         <div className="card-body">
           <h5 className="card-title">{props.data.title}</h5>
           <a href={props.data.link} target="_blank" className="btn btn-primary">
